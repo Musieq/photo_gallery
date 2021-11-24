ddc2235fe5a57435a5125729a4c3c060b5c1ba9f
Musieq <m.musielak97@gmail.com>
2021-11-24 18:52:41 +0100

    Initial commit

---
+++ project/includes/footer.php
@@ -0,0 +1,12 @@
+<!-- Footer-->
+<footer class="py-5 bg-dark">
+    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+</footer>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+<!-- Bootstrap core JS-->
+<script src="js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+</body>
+</html>
