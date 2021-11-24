ab284aa9aaf23775b8719038034402c738d641e2
Musieq <m.musielak97@gmail.com>
2021-11-24 19:48:35 +0100

    Prepared files

---
+++ project/admin/comments.php
@@ -0,0 +1,24 @@
+<?php
+include 'includes/header.php';
+?>
+    <div id="layoutSidenav">
+
+    <!-- Sidenav -->
+<?php
+include 'includes/sidenav.php';
+?>
+
+    <!-- Page content -->
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4">
+                <h1 class="mt-4">Comments</h1>
+                <ol class="breadcrumb mb-4">
+                    <li class="breadcrumb-item active">Comments</li>
+                </ol>
+
+
+            </div>
+        </main>
+<?php
+include 'includes/footer.php';
---
+++ project/admin/includes/sidenav.php
@@ -0,0 +1,44 @@
+<div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <div class="sb-sidenav-menu-heading">Core</div>
+                <a class="nav-link" href="index.php">
+                    <div class="sb-nav-link-icon"><i class="bi bi-speedometer2"></i></div>
+                    Dashboard
+                </a>
+                <a class="nav-link" href="users.php">
+                    <div class="sb-nav-link-icon"><i class="bi bi-people"></i></div>
+                    Users
+                </a>
+                <a class="nav-link" href="upload.php">
+                    <div class="sb-nav-link-icon"><i class="bi bi-images"></i></div>
+                    Upload image
+                </a>
+                <a class="nav-link" href="comments.php">
+                    <div class="sb-nav-link-icon"><i class="bi bi-chat"></i></div>
+                    Comments
+                </a>
+
+
+
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon"><i class="bi bi-images"></i></div>
+                    Images
+                    <div class="sb-sidenav-collapse-arrow"><i class="bi bi-arrow-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="images.php">Show images</a>
+                        <a class="nav-link" href="upload.php">Upload image</a>
+                    </nav>
+                </div>
+
+            </div>
+        </div>
+        <div class="sb-sidenav-footer">
+            <div class="small">Logged in as:</div>
+            Start Bootstrap
+        </div>
+    </nav>
+</div>
