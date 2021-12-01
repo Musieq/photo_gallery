f7028e84fd91b91cb4eb4c8fc086cb0535a38245
Musieq <m.musielak97@gmail.com>
2021-12-01 17:05:54 +0100

    show users in admin

--- project/admin/users.php
+++ project/admin/users.php
@@ -1,5 +1,8 @@
 <?php
 include 'includes/header.php';
+
+/** User init **/
+$user = new User();
 ?>
     <div id="layoutSidenav">
 
@@ -14,9 +17,40 @@ include 'includes/sidenav.php';
             <div class="container-fluid px-4">
                 <h1 class="mt-4">Users</h1>
                 <ol class="breadcrumb mb-4">
-                    <li class="breadcrumb-item active">Users</li>
+                    <li class="breadcrumb-item active">All users</li>
                 </ol>
 
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Username</th>
+                                <th scope="col">Login</th>
+                                <th scope="col">Creation date</th>
+                                <th scope="col">Role</th>
+                            </tr>
+                        </thead>
+                        
+                        <tbody>
+                        <?php
+                        $allUsers = $user->getAllUsers();
+
+                        foreach ($allUsers as $row) {
+                            ?>
+                            <tr>
+                                <td><?=$row['username']?></td>
+                                <td><?=$row['login']?></td>
+                                <td><?=$row['creation_date']?></td>
+                                <td><?=$row['role']?></td>
+                            </tr>
+                            <?php
+                        }
+
+                        //TODO: pagination
+                        ?>
+                        </tbody>
+                    </table>
+                </div>
 
             </div>
         </main>
