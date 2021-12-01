7f598b9e0e78ff7255d7210dca9aff9cf0c4f240
Musieq <m.musielak97@gmail.com>
2021-12-01 17:05:45 +0100

    get single user

--- project/admin/includes/class/user.php
+++ project/admin/includes/class/user.php
@@ -9,4 +9,9 @@ class User {
         return $db->query('SELECT * FROM users')->fetchAll();
     }
 
+    public function getUser($id): array {
+        global $db;
+
+        return $db->query('SELECT * FROM users WHERE id = ?', $id)->fetchAll();
+    }
 }
